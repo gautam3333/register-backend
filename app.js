6475119a0432b4e3f902b5de5a74ebbc7fa26c48
Gautam Sarkar <gautam.sarkar3333@gmail.com>
2023-11-17 18:49:45 +0530

    fix: add CORS

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const registrationRouter = require('./routes/registrationRoutes');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/v1/registrations', registrationRouter);
+app.use(
+  'https://register-backend-vxw4.onrender.com/api/v1/registrations',
+  registrationRouter
+);
 
 module.exports = app;
