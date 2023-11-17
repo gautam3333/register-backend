a995554eec68a453b596db2a4237aa199a4ea9af
Gautam Sarkar <gautam.sarkar3333@gmail.com>
2023-11-17 19:48:12 +0530

    fix: remove CORS package

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const registrationRouter = require('./routes/registrationRoutes');
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 app.use('/api/v1/registrations', registrationRouter);
 
